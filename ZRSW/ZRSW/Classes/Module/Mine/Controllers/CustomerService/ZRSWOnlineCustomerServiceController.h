53f4840350d838f8a5921dd7fca573b9351ad07a
黄亚男 <huangyn2@lenovo.com>
2018-09-27 17:40:18 +0800

    集成环信 初步实现在线客服

---
+++ ZRSW/ZRSW/Classes/Module/Mine/Controllers/CustomerService/ZRSWOnlineCustomerServiceController.h
@@ -0,0 +1,13 @@
+//
+//  ZRSWOnlineCustomerServiceController.h
+//  ZRSW
+//
+//  Created by King on 2018/9/27.
+//  Copyright © 2018年 周培玉. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ZRSWOnlineCustomerServiceController : BaseViewController
+
+@end
